823c81a76fb53f34ed651afe2adeb6de115de3e1
Joshua Lewis <joshlewi14@gmail.com>
2022-03-07 19:48:49 -0600

    Small formatting changes

--- src/main/java/frc/team8732/robot/subsystems/Intake.java
+++ src/main/java/frc/team8732/robot/subsystems/Intake.java
@@ -8,49 +8,45 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import frc.team8732.lib.drivers.TalonSRXFactory;
 import frc.team8732.robot.Constants;
  
-/** Add your docs here. */
+/** This is the Intake subsystem for the 2022 robot. It contains all the information 
+ * (variables, constructors, methods etc.) in order to allow the shooter to function.*/
 public class Intake extends Subsystem {
   private static Intake mInstance; 
-//Hardware
-private final TalonSRX mSpicyIntake;
+    //Hardware
+    private final TalonSRX mSpicyIntake;
 
-    public static final double mFastIntakeSpeed = .8;
-    public static final double mSlowIntakeSpeed = .3; 
+        public static final double mFastIntakeSpeed = .8;
+        public static final double mSlowIntakeSpeed = .3; 
 
-//get Instances
-public synchronized static Intake getInstance() {
-    if (mInstance == null){
-        mInstance = new Intake();
+    //get Instances
+    public synchronized static Intake getInstance() {
+        if (mInstance == null){
+            mInstance = new Intake();
+        }
+        return mInstance;
     }
-      return mInstance;
-}
 
-public Intake(){
-    mSpicyIntake = TalonSRXFactory.createDefaultTalon(Constants.kIntakeID);
-    }
+    public Intake(){
+        mSpicyIntake = TalonSRXFactory.createDefaultTalon(Constants.kIntakeID);
+        }
 
     public void setIntakeSpeedPercent(double percentOutput){
         mSpicyIntake.set(ControlMode.PercentOutput, percentOutput);
-      
-     }
+        
+        }
 
-@Override
-public void stop(){
-    mSpicyIntake.set(ControlMode.PercentOutput, 0.0);
- 
-    //TODO Auto-generated method stub
-}
- 
-@Override
-public boolean checkSystem(){
-    return false;  
-    //TODO Auto-generated method stub
-}
-@Override
-public void outputTelemetry(){
-    ////TODO Auto-generated method stub
-}
- 
+    @Override
+    public void stop(){
+        mSpicyIntake.set(ControlMode.PercentOutput, 0.0);
+    }
+    
+    @Override
+    public boolean checkSystem(){
+        return false;  
+    }
+    @Override
+    public void outputTelemetry(){
+    }
 }
 
 
